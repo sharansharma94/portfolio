0d59a04554dc1e5d61266ed2cb820c5a45f1ff7c
sharan sharma <sharansharma19@gmail.com>
2021-11-18 22:16:58 +0530

    Pages added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import GlobalStyle from "./globalStyles";
 import { ThemeProvider } from "styled-components";
 import { darkTheme } from "./components/Themes";
+import { Route, Switch } from "react-router";
+import Main from "./components/Main";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Work from "./components/pages/Work";
+import Skills from "./components/pages/Skills";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        App File
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/skills" component={Skills} />
+        </Switch>
       </ThemeProvider>
     </>
   );
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <>Main Component</>;
+};
+
+export default Main;
