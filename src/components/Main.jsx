99536d98e60b94d50631b59481d070d7f6cd464a
sharan sharma <sharansharma19@gmail.com>
2021-11-20 22:51:50 +0530

    Work blog added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../common/Logo";
 import PowerButton from "../common/PowerButton";
@@ -5,7 +6,7 @@ import Socials from "../common/Socials";
 
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
-  width: 100vh;
+  width: 100vw;
   height: 100vh;
   overflow: hidden;
 
@@ -24,6 +25,35 @@ const MainContainer = styled.div`
 const Container = styled.div`
   padding: 2rem;
 `;
+
+const Contact = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 3;
+`;
+
+const Blog = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const Work = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  left: 2rem;
+  transform: translate(-50%, -50%) rotate(90deg);
+  text-decoration: none;
+  z-index: 1;
+`;
 const Main = () => {
   return (
     <>
@@ -32,6 +62,20 @@ const Main = () => {
           <PowerButton />
           <Logo />
           <Socials />
+          <Contact
+            target="_blank"
+            to={{ pathname: "mailto:sharmasharan94@outlook.com" }}
+          >
+            <h3>Say hi..</h3>
+          </Contact>
+
+          <Blog to="/blog">
+            <h3>Blog</h3>
+          </Blog>
+
+          <Work to="/blog">
+            <h3>Work</h3>
+          </Work>
         </Container>
       </MainContainer>
     </>
