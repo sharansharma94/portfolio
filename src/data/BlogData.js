a653ddb795b73328402ae337781483375e6f0963
sharan sharma <sharansharma19@gmail.com>
2021-11-27 23:15:09 +0530

    Blog added

---
+++ src/data/BlogData.js
@@ -0,0 +1,43 @@
+export const Blogs = [
+  {
+    id: 1,
+    name: "Test React 1",
+    tags: ["react", "gsap", "styled-components"],
+    date: "13 May, 2021",
+    imgSrc: "image.png",
+    link: "/",
+  },
+
+  {
+    id: 2,
+    name: "Test React 1",
+    tags: ["react", "gsap", "styled-components"],
+    date: "13 May, 2021",
+    imgSrc: "image.png",
+    link: "/",
+  },
+  {
+    id: 3,
+    name: "Test React 1",
+    tags: ["react", "gsap", "styled-components"],
+    date: "13 May, 2021",
+    imgSrc: "image.png",
+    link: "/",
+  },
+  {
+    id: 4,
+    name: "Test React 1",
+    tags: ["react", "gsap", "styled-components"],
+    date: "13 May, 2021",
+    imgSrc: "image.png",
+    link: "/",
+  },
+  {
+    id: 5,
+    name: "Test React 1",
+    tags: ["react", "gsap", "styled-components"],
+    date: "13 May, 2021",
+    imgSrc: "image.png",
+    link: "/",
+  },
+];
