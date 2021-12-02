213e09f6d59a92e8d2bbe744548035195098cd37
sharan sharma <sharansharma19@gmail.com>
2021-12-02 16:01:29 +0530

    Particle and text added

---
+++ src/common/Particles.jsx
@@ -0,0 +1,24 @@
+import Particles from "react-particles-js";
+import configDark from "../config/particlesjs-config.json";
+import configLight from "../config/particlesjs-config-light.json";
+import styled from "styled-components";
+
+const Box = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 0;
+`;
+
+export default function ParticlesComponent(props) {
+  return (
+    <Box>
+      <Particles
+        style={{ position: "absolute", top: 0 }}
+        params={props.theme === "light" ? configLight : configDark}
+      />
+    </Box>
+  );
+}
