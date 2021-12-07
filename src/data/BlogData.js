9b0f138a436209eec2193db633da40aed1d7a0c9
sharan sharma <sharansharma19@gmail.com>
2021-12-07 22:31:22 +0530

    work added

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,11 +1,12 @@
 export const Blogs = [
   {
     id: 1,
-    name: "Test React 1",
-    tags: ["react", "gsap", "styled-components"],
-    date: "13 May, 2021",
-    imgSrc: "image.png",
-    link: "/",
+    name: "Google Interview problem solution",
+    tags: ["javascript", "interview", "google", "data structure"],
+    date: "3 Sep, 2021",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--Pk9hCH2a--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/1pd4uo0chtligd37hkma.png",
+    link: "https://dev.to/sharansharma94/google-interview-problem-solution-ae2",
   },
 
   {
